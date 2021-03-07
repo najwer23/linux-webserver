e7d9bf024edcbbd86efe6c1d49cc2fc0bef6d38a
Travis CI <najwer23@gmail.com>
2021-03-07 09:47:32 +0100

    Corrected script staticip.sh

--- staticip.sh
+++ staticip.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # chmod 755 staticip.sh
-# ./staticip.sh
+# sudo ./staticip.sh
+# sudo nano /etc/network/interfaces
 
-cat << EOF >> sudo /etc/network/interfaces
+cat << EOF >> /etc/network/interfaces
 iface eth0 inet static
 address 192.168.10.5
 netmask 255.255.255.0
 gateway 192.168.10.1
 network	192.168.10.0
 broadcast 192.168.10.255
-test
 EOF
